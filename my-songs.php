161b7ba0a01d5a1f364fdcd767313e41e2048574
Pantherinae <blade_master_pantherinae@hotmail.com>
2019-03-09 11:43:22 -0700

    fix html tags + removed redundant code

--- my-songs.php
+++ my-songs.php
@@ -1,42 +1,21 @@
 <html>
-<head>
-</head>
-<center>
-<body>
-	
-	<?php
-	//session_start();
-	//Gets the name from POST and stores it in SESSION
-	$_SESSION['name'] = $_POST['name'];
-	echo $_SESSION['name']."'s Playlist";
-	//This will be the file that holds the songs owned by the user
-	$file = "users/".$_SESSION['name']."/".$_SESSION['name'].".txt";
-	if(!is_dir("users/".$_SESSION['name'])){
-		$oldmask = umask(0);
-		mkdir("users/".$_SESSION['name'],0777);
-		mkdir("users/".$_SESSION['name']."/playlists",0777);
-		umask($oldmask);
-		$fp=fopen($file,"a");
-		
-	}
-	$fp = fopen($file,"name");
-		foreach($_POST['song'] as $s){
-		fputs($fp, $s."\n");
- }
-	
-	
-	?>
-	
-	<h1> Buy songs from Here:</h1>
-	<form action="zmazon.php" method=POST>
-		<input type='radio' name='playlist' value='zmazon'> zmazon<br>
-		<button type="submit" class="btn btn-primary">Go</button>
-	</form>
-	<form action="ztunes.php" method=POST>
-		<input type='radio' name='playlist' value='ztunes'> ztunes<br>
-		<button type="submit" class="btn btn-primary">Go</button>
-	</form>
-	
+    <body>
+    <center>
+        <?php
+        //session_start();
+        //Gets the name from POST and stores it in SESSION
+        $_SESSION['name'] = $_POST['name'];
+        echo $_SESSION['name'] . "'s Playlist";
+        ?>
+        <h1> Buy songs from Here:</h1>
+        <form action="zmazon.php" method=POST>
+            <input type='radio' name='song' value='zmazon'> zmazon<br>
+            <button type="submit" class="btn btn-primary">Go</button>
+        </form>
+        <form action="ztunes.php" method=POST>
+            <input type='radio' name='song' value='ztunes'> ztunes<br>
+            <button type="submit" class="btn btn-primary">Go</button>
+        </form>
+    </center>
 </body>
-</center>
 </html>
--- newMusic.php
+++ newMusic.php
@@ -1,36 +1,23 @@
 <html>
-	<head>
-	<?php
-		//function clearform(){
-			//document.getElementById("song").value =""; 
-	
-	?>
-	</head>
-	<center>
-	<body style="background-color:powderblue;">
-	<h1>My Music Store</h1>
-	<img src="images.jpg" width="70" height="60">
-	<br/>
-	<br/>
-	<h1> Log In</h1>
-	<form action="my-songs.php" method=POST>
-        <input type='radio' name='name' value='Leslie'> Leslie<br>
-		<input type='radio' name='name' value='Kelly'> Kelly<br>
-        <button type="submit" class="btn btn-primary">Log In</button>
-    </form>
-    <form action="my-songs.php" method=POST>
+    <head>
+    </head>
+    <body style="background-color:powderblue;">
+    <center>
+        <h1>My Music Store</h1>
+        <img src="images.jpg" width="70" height="60">
+        <br/>
+        <br/>
+        <h1> Log In</h1>
+        <form action="my-songs.php" method=POST>
+            <input type='radio' name='name' value='Leslie'> Leslie<br>
+            <input type='radio' name='name' value='Kelly'> Kelly<br>
+            <button type="submit" class="btn btn-primary">Log In</button>
+        </form>
+        <form action="my-songs.php" method=POST>
             <label>Name:</label>
             <input type="text" class="form-control" name="name" placeholder="Enter your name">
-        <button type="submit" class="btn btn-primary">Create New User</button>
-    </form>
-	
-
-		
-	
-	
-	
-	</body>
-	</center>
-	
-
+            <button type="submit" class="btn btn-primary">Create New User</button>
+        </form>
+    </center>    
+</body>
 </html>

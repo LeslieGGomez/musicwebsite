44ddd5f29f90c029e6e8037a337354c6c113ae7e
LeslieGGomez <43277542+LeslieGGomez@users.noreply.github.com>
2019-03-08 18:51:38 -0700

    added files

---
+++ ztunes.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<h1> Welcome to zTunes Playlist</h1>
+<form action="my-songs.php" method=POST>
+        <input type='radio' name='song' value='Song1'> Song1<br>
+		<input type='radio' name='song' value='Song2'> Song2<br>
+        <button type="submit" class="btn btn-primary">buy</button>
+    </form>
+</body>
+</html>

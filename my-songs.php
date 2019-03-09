44300bf1a3f4077c154853bb3d8f2eac3f7c0b57
Pantherinae <blade_master_pantherinae@hotmail.com>
2019-03-09 12:58:41 -0700

    fixed .txt file and dir issues + saved username information throught the project

--- my-songs.php
+++ my-songs.php
@@ -2,43 +2,42 @@
     <body>
     <center>
         <?php
-        //session_start();
-        //Gets the name from POST and stores it in SESSION
-        $_SESSION['name'] = $_POST['name'];
-        echo $_SESSION['name'] . "'s Playlist";
-        //This will be the file that holds the songs owned by the user
-        $file = "users/" . $_SESSION['name'] . "/" . $_SESSION['name'] . ".txt";
-        if (!is_dir("users/" . $_SESSION['name'])) {
-            $oldmask = umask(0);
-            mkdir("writeable/" . $_SESSION['name'], 0777);
-            mkdir("writeable/" . $_SESSION['name'] . "/playlists", 0777);
-            umask($oldmask);
-            $fp = fopen($file, "a");
+        //Gets the name from POST if empty and stores it in SESSION
+        if (!isset($_SESSION['name'])) {
+            $_SESSION['name'] = $_POST['name'];
         }
-        //Add songs
-        $fp = fopen($file, "a"); //create file with name and opens file for write
-        if (isset($_POST['song'])) {
-            $record = $_POST['song'];
-            foreach ($record as $song) {
-                fwrite($fp, "$song");
-                if (strstr($song, "\n")) {
-                    fwrite($fp, "song\n");
-                } else {
-                    fwrite($fp, "$song\n");
-                }
+        echo $_SESSION['name'] . "'s Playlist";
+        //Hold all needed information from username, dir, and .txt
+        $username = $_POST['name'];
+        $filedir = "writeable/" . "playlist/";
+        $textpath = $filedir . $username . ".txt";
+        //Creates a filepath and a .txt of the user if non-existent
+        if (!is_dir($filedir)) {
+            mkdir("writeable/", 0777);
+            mkdir($filedir, 0777);
+            if (!file_exists($textpath)) {
+                $fp = fopen($textpath, "a");
             }
         }
+        $fp = fopen($textpath, "a");
+        if (isset($_POST['song'])) {
+            fwrite($fp, "$song\n");
+        }
         fclose($fp);
-        $_SESSION['name'] = $_POST['name'];
-        echo $_SESSION['name'] . "'s Playlist";
         ?>
         <h1> Buy songs from Here:</h1>
         <form action="zmazon.php" method=POST>
             <input type='radio' name='song' value='zmazon'> zmazon<br>
+            <?php
+            echo "<input type=\"hidden\" name=\"name\" value=\"" . $_SESSION['name'] . "\" />";
+            ?>
             <button type="submit" class="btn btn-primary">Go</button>
         </form>
         <form action="ztunes.php" method=POST>
             <input type='radio' name='song' value='ztunes'> ztunes<br>
+            <?php
+            echo "<input type=\"hidden\" name=\"name\" value=\"" . $_SESSION['name'] . "\" />";
+            ?>
             <button type="submit" class="btn btn-primary">Go</button>
         </form>
     </center>

f83a78fc4ae2a0c0b0f7581da6d054c30e326641
LeslieGGomez <lggomez4@miners.utep.edu>
2019-03-10 12:22:48 -0600

    added array

--- playlist.php
+++ playlist.php
@@ -1,10 +1,14 @@
-<html>
-	<body>
+
 		<?php
 		session_start();
 		$_SESSION['name'] = "";
 		 echo $_SESSION['name'] . "'s Playlist";
+		 $playlistArr = array("song1" => song1);
+		 echo $playlistArr['song1']. "<br/>";
+		 $_SESSION['playlist'] = $playlistArr;
 		?>
+<html>
+	<body>
 	</body>
 </html>
 		
